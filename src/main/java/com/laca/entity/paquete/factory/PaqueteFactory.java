93efd81b0721e2655d45016b5bdd48875ddb8231
Ian-Asch <74163133+Ian-Asch@users.noreply.github.com>
2024-03-26 19:15:06 -0600

    Update PaqueteFactory.java

--- src/main/java/com/laca/entity/paquete/factory/PaqueteFactory.java
+++ src/main/java/com/laca/entity/paquete/factory/PaqueteFactory.java
@@ -6,4 +6,8 @@ public class PaqueteFactory {
     public static Paquete creacrPaquete() {
         return new Paquete();
     }
+
+    public static Paquete creacrPaquete(String tipo, double peso, String nombre, String Descripcion, double precio, double altura, double ancho) {
+        return new Paquete(tipo,peso,nombre,Descripcion,precio,altura,ancho);
+    }
 }
