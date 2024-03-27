21bd474c45755f1d45263ffc1e5b959bcc6e83de
Ian-Asch <74163133+Ian-Asch@users.noreply.github.com>
2024-03-26 19:09:24 -0600

    Paquete

---
+++ src/main/java/com/laca/entity/paquete/factory/PaqueteFactory.java
@@ -0,0 +1,9 @@
+package com.laca.entity.paquete.factory;
+
+import com.laca.entity.paquete.objects.Paquete;
+
+public class PaqueteFactory {
+    public static Paquete creacrPaquete() {
+        return new Paquete();
+    }
+}
---
+++ src/main/java/com/laca/entity/paquete/objects/Paquete.java
@@ -0,0 +1,13 @@
+package com.laca.entity.paquete.objects;
+
+public class Paquete {
+    String tipo;
+    double peso;
+    String nombre;
+    String Descripcion;
+    double precio;
+    double altura;
+    double ancho;
+
+    public Paquete() {}
+}
