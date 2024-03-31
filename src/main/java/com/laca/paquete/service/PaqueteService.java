efa54a4ee89576492d3ec7b9b2414f8ae3b6d132
Ian-Asch <74163133+Ian-Asch@users.noreply.github.com>
2024-03-31 17:17:23 -0600

    paquete service

--- src/main/java/com/laca/paquete/service/PaqueteService.java
+++ src/main/java/com/laca/paquete/service/PaqueteService.java
@@ -53,4 +53,71 @@ public class PaqueteService {
 
         return paquetes;
     }
+
+
+    @Transactional
+    public Paquete savePaquete(Paquete paquete) {
+
+        try (Connection connection = dataSource.getConnection()) {
+            String query = "INSERT INTO Packages (PackageID," +
+                    "Name," +
+                    "Description," +
+                    "Weight," +
+                    "Price," +
+                    "SizeHeight," +
+                    "SizeWidth," +
+                    "ClientID," +
+                    "RouteID," +
+                    "Status) VALUES (?,?,?,?,?,?,?,?,?,?)";
+
+            PreparedStatement statement = connection.prepareStatement(query,PreparedStatement.RETURN_GENERATED_KEYS);
+
+            statement.setInt(1, paquete.getPackageID());
+            statement.setString(2, paquete.getName());
+            statement.setString(3, paquete.getDescription());
+            statement.setDouble(4, paquete.getWeight());
+            statement.setDouble(5, paquete.getPrice());
+            statement.setDouble(6, paquete.getSizeHeight());
+            statement.setDouble(7, paquete.getSizeWidth());
+            statement.setInt(8, paquete.getClientID());
+            statement.setInt(9, paquete.getRouteID());
+            statement.setString(10, paquete.getStatus());
+
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows == 1) {
+                ResultSet generatedKeys = statement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    paquete.setPackageID(generatedKeys.getInt(1));
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return paquete;
+    }
+
+
+    @Transactional
+    public Paquete updatePaquete(int packageID, Paquete updatePaquete) {
+
+        try (Connection connection = dataSource.getConnection()) {
+
+        String query = "UPDATE Packages SET Name=?," +
+                "Description=?," +
+                "Weight=?," +
+                "Price=?," +
+                "SizeHeight=?," +
+                "SizeWidth=?," +
+                "ClientID=?," +
+                "RouteID=?," +
+                "Status=? WHERE PackageID=?";
+
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error updating user: " + e.getMessage(), e);
+        }
+    }
 }
